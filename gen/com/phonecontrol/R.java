67c3ec3996545aaf257079fff7b52f465d35b5da
lgkern <lucasgilbertokern@gmail.com>
2014-06-23 22:22:11 -0300

    Sensor Calibration
    Added a working sensor calibration.

--- gen/com/phonecontrol/R.java
+++ gen/com/phonecontrol/R.java
@@ -24,12 +24,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int TextView01=0x7f08000b;
         public static final int action_settings=0x7f08000c;
         public static final int button1=0x7f080009;
         public static final int button2=0x7f08000a;
         public static final int textView1=0x7f080000;
         public static final int textView10=0x7f080008;
-        public static final int textView2=0x7f08000b;
         public static final int textView3=0x7f080001;
         public static final int textView4=0x7f080002;
         public static final int textView5=0x7f080003;
--- src/com/phonecontrol/Configuration.java
+++ src/com/phonecontrol/Configuration.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -22,8 +23,10 @@ public class Configuration extends Activity implements SensorEventListener {
 	private Sensor senAccelerometer;
 	private Sensor senGyroscope;
 	private Sensor senGravity;
-	
+
 	private boolean logging = false;
+	
+	private long lastClick = 0;
 		
 	private long refreshRate = 100;
 	private long lastUpdate = 0;
@@ -33,6 +36,16 @@ public class Configuration extends Activity implements SensorEventListener {
 	private float ac_x, ac_y, ac_z;
 	private float gy_x, gy_y, gy_z;
 	private float gr_x, gr_y, gr_z;
+	
+	private float[] acAccelerometer;
+	private float[] acGyroscope;
+	
+	private float[] ac_LastVelocity;
+	private float[] gy_LastVelocity;	
+	
+	private float[] ac_lastState;
+	private float[] gy_lastState;
+	
 	private static final int SHAKE_THRESHOLD = 600;
 	
     @Override
@@ -45,9 +58,9 @@ public class Configuration extends Activity implements SensorEventListener {
         senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
         senGyroscope = senSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
         senGravity = senSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
-        senSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
-        senSensorManager.registerListener(this, senGyroscope , SensorManager.SENSOR_DELAY_NORMAL);
-        senSensorManager.registerListener(this, senGravity , SensorManager.SENSOR_DELAY_NORMAL);
+        //senSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
+        //senSensorManager.registerListener(this, senGyroscope , SensorManager.SENSOR_DELAY_NORMAL);
+        //senSensorManager.registerListener(this, senGravity , SensorManager.SENSOR_DELAY_NORMAL);
         
         Button btnStart = (Button) findViewById(R.id.button1);
         Button btnStop = (Button) findViewById(R.id.button2);
@@ -66,6 +79,61 @@ public class Configuration extends Activity implements SensorEventListener {
         	}
         });
     }
+    
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event){
+
+    	if(keyCode == KeyEvent.KEYCODE_VOLUME_DOWN){
+        	long currentClick = System.currentTimeMillis(); 
+    		if(currentClick - lastClick > 1000){    			
+	    		resetAcumulators();
+	            senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+	            senSensorManager.registerListener(this, senGyroscope , SensorManager.SENSOR_DELAY_NORMAL);
+	            senSensorManager.registerListener(this, senGravity , SensorManager.SENSOR_DELAY_NORMAL);
+	            TextView results = (TextView)findViewById(R.id.TextView01);
+	            ac_lastState = new float[3];
+	            gy_lastState = new float[3];
+	            results.setText("Live logging");
+    		}
+    		lastClick = currentClick;
+    		return true;
+    	
+    	}
+    	return false;
+    }
+    
+    private void resetAcumulators() {
+    	ac_LastVelocity = new float[3];
+    	gy_LastVelocity = new float[3];
+    	acAccelerometer = new float[3];
+    	acGyroscope = new float[3];
+	}
+
+	@Override
+    public boolean onKeyUp(int keyCode, KeyEvent event){
+    	if(keyCode == KeyEvent.KEYCODE_VOLUME_DOWN){    		
+            senSensorManager.unregisterListener(this);  
+            TextView acTextX = (TextView)findViewById(R.id.textView1);
+            TextView acTextY = (TextView)findViewById(R.id.textView3);
+            TextView acTextZ = (TextView)findViewById(R.id.textView4);
+            TextView gyTextX = (TextView)findViewById(R.id.textView5);
+            TextView gyTextY = (TextView)findViewById(R.id.textView6);
+            TextView gyTextZ = (TextView)findViewById(R.id.textView7);
+            TextView results = (TextView)findViewById(R.id.TextView01);
+            results.setText("Acumulator Results");
+            
+            acTextX.setText("Ac x:" + Float.toString(acAccelerometer[0]));
+            acTextY.setText("Ac y:" + Float.toString(acAccelerometer[1]));
+            acTextZ.setText("Ac z:" + Float.toString(acAccelerometer[2]));
+            
+            gyTextX.setText("Gy x:" + Float.toString(acGyroscope[0]));
+            gyTextY.setText("Gy y:" + Float.toString(acGyroscope[1]));
+            gyTextZ.setText("Gy z:" + Float.toString(acGyroscope[2]));
+    		return true;
+    	}
+    	return false;
+    }
+
 
 
     @Override
@@ -102,6 +170,8 @@ public class Configuration extends Activity implements SensorEventListener {
                 ac_x = x;
                 ac_y = y;
                 ac_z = z;
+                
+                updateDistance(0, x, y, z, diffTime);
                 xtext.setText("Ac x:" + Float.toString(x));
                 ytext.setText("Ac y:" + Float.toString(y));
                 ztext.setText("Ac z:" + Float.toString(z));
@@ -131,6 +201,7 @@ public class Configuration extends Activity implements SensorEventListener {
                  gy_x = x;
                  gy_y = y;
                  gy_z = z;
+                 updateDistance(1, x, y, z, diffTime);
                  xtext.setText("Gy x:" + Float.toString(x));
                  ytext.setText("Gy y:" + Float.toString(y));
                  ztext.setText("Gy z:" + Float.toString(z));
@@ -157,7 +228,7 @@ public class Configuration extends Activity implements SensorEventListener {
                 if (speed > SHAKE_THRESHOLD) {
      
                 }
-                
+                  
                 gr_x = x;
                 gr_y = y;
                 gr_z = z;
@@ -177,8 +248,66 @@ public class Configuration extends Activity implements SensorEventListener {
 	        data += "ac: " + Float.toString(ac_x) + "\t" + Float.toString(ac_y) + "\t" + Float.toString(ac_z) + "\t";
 	        data += "gy: " + Float.toString(gy_x) + "\t" + Float.toString(gy_y) + "\t" + Float.toString(gr_z) + "\t";
 	        data += "gr: " + Float.toString(gr_x) + "\t" + Float.toString(gr_y) + "\t" + Float.toString(gy_z) + "\n";
-	        writeToFile(data);
+	     //   writeToFile(data);
 	    }
+    }
+    
+    
+    private void updateDistance(int storeVariable, float x, float y, float z, long timeFrame){
+    	float X = (float) Math.sqrt(x*x);
+    	float Y = (float) Math.sqrt(y*y);
+    	float Z = (float) Math.sqrt(z*z);
+    	float tolerance = 0.0f;    	
+    	float[] positions = new float[3];    
+    	float s;
+    	float u;
+    	float[] velocity = new float[3];
+    	float a = 0.0f;
+    	for(int i = 0; i < 3; i++){
+    		if(i == 0) { 
+    			if (X > (float)tolerance*Math.sqrt((storeVariable-1)*(storeVariable-1))) 
+    				a = exponentialSmoothing(storeVariable, i, X); 
+    			else
+    				a = 0.0f;
+    		}
+    		if(i == 1) {
+    			if (Y > (float)tolerance*Math.sqrt((storeVariable-1)*(storeVariable-1))) 
+    				a = exponentialSmoothing(storeVariable, i, Y); 
+    			else 
+    				a = 0.0f;
+    		}
+    		if(i == 2) {
+    			if (Z > (float)tolerance*Math.sqrt((storeVariable-1)*(storeVariable-1))) 
+    				a = exponentialSmoothing(storeVariable, i, Z);
+    			else 
+    				a = 0.0f;
+    			}
+        	if(storeVariable == 0){ //accelerometer
+        		u = ac_LastVelocity[i];
+        		s = acAccelerometer[i];
+        	}
+        	else{
+        		u = gy_LastVelocity[i];
+        		s = acGyroscope[i];
+        	}
+        	velocity[i] = u + (a*timeFrame);
+        	positions[i] = a*timeFrame;//s + (velocity[i] * timeFrame);        
+    	}
+    	if(storeVariable == 0){
+    		for(int i = 0; i < 3; i++){
+    			ac_LastVelocity[i] = velocity[i];
+        		acAccelerometer[i] = positions[i];
+    		}    		
+    	}	
+    	else{
+    		for(int i = 0; i < 3; i++){
+    			gy_LastVelocity[i] = velocity[i];
+    			acGyroscope[i] = positions[i];
+    		}
+    	}
+    	
+        TextView results = (TextView)findViewById(R.id.TextView01);
+        results.setText(Float.toString(acAccelerometer[0]));
     }
 		
      
@@ -194,25 +323,41 @@ public class Configuration extends Activity implements SensorEventListener {
     
     protected void onResume() {
         super.onResume();
-        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-        senSensorManager.registerListener(this, senGyroscope , SensorManager.SENSOR_DELAY_NORMAL);
-        senSensorManager.registerListener(this, senGravity , SensorManager.SENSOR_DELAY_NORMAL);
+
     }
     
+    private float exponentialSmoothing(int method, int variable, float output) {
+    	float alpha = 0.2f;
+    	float input = 0.0f;
+    	if(method == 0){
+    		input = ac_lastState[variable];
+    		output = output + alpha * (input - output);
+    		ac_lastState[variable] = output;
+    	}
+    	else{
+    		input = gy_lastState[variable];
+    		output = output + alpha * (input - output);
+    		gy_lastState[variable] = output;
+    	}
+        
+        
+        return output;
+}
+/*    
     private void writeToFile(String data) {
         try {
-    		TextView logText = (TextView)findViewById(R.id.textView2);
+    	//	TextView logText = (TextView)findViewById(R.id.textView2);
     		logText.setText("Writing to file");
             OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput("config.txt", Context.MODE_PRIVATE));
             outputStreamWriter.write(data);
             outputStreamWriter.close();
         }
         catch (IOException e) {
-        	TextView logText = (TextView)findViewById(R.id.textView2);
+        //	TextView logText = (TextView)findViewById(R.id.textView2);
             Log.e("Exception", "File write failed: " + e.toString());
             logText.setText("File write failed:" + e.toString());
         } 
     }
-	
+	*/
 } 
 
